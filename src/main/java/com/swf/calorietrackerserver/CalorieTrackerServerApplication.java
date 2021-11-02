629a1a7950bbf68f986581fe2f9957f10e0f5216
Ben Hunter <learntocode@benhunter.me>
2021-11-02 09:45:17 -0500

    Initial setup.

---
+++ src/main/java/com/swf/calorietrackerserver/CalorieTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.swf.calorietrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieTrackerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swf/calorietrackerserver/CalorieTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swf.calorietrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
